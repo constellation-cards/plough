fb3e8de8071e6e5299bab37dc84d53d18d72ba79
Bill Garrett <garrett@astralfrontier.org>
2021-12-19 14:14:40 -0800

    Serve the SPA for game URLs

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,7 @@
 import { Server } from "colyseus"
+import express, { Request, Response } from "express"
 import { createServer } from "http"
-import express from "express"
+import path from "path"
 
 import { ConstellationCardsRoom } from "../constellation-cards/room"
 
@@ -9,6 +10,14 @@ const port = Number(process.env.port) || 3000
 const app = express()
 app.use(express.json())
 app.use(express.static("public"))
+
+const serveStatic = (req: Request, res: Response) => {
+    console.log(req.path)
+    res.sendFile(path.join(path.resolve("public"), "index.html"))
+}
+
+app.get('/game', serveStatic)
+app.get('/game/:gameId', serveStatic)
 
 const gameServer = new Server({
     server: createServer(app),
