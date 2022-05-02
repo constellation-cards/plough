8aed861e115da578b06f81f3719362d343877032
Bill Garrett <garrett@astralfrontier.org>
2022-05-01 23:55:53 -0700

    Spruce up home page

--- src/constellation-cards/client/ConstellationCardsHome.tsx
+++ src/constellation-cards/client/ConstellationCardsHome.tsx
@@ -1,4 +1,4 @@
-import { Container } from "@mui/material"
+import { Button, Container } from "@mui/material"
 import React from "react"
 import { Link } from "react-router-dom"
 
@@ -11,7 +11,24 @@ export default (props: ConstellationCardsHomeProps) => {
         <Container>
             <h1>Constellation Cards</h1>
 
-            <Link to="/game">Play</Link>
+            <Button variant="contained" component={Link} to="/game">
+                Play
+            </Button>
+
+            <p>
+                This is the <strong>Plough</strong> game server,
+                a custom Virtual Table Top (VTT) designed to play
+                Constellation Cards.
+            </p>
+
+            <p>
+                Click "Play" to jump into the demo game session!
+            </p>
+
+            <ul>
+                <li><a href="https://constellation.cards/">constellation.cards</a></li>
+                <li><a href="https://github.com/constellation-cards/plough">Github</a></li>
+            </ul>
         </Container>
     )
 }
